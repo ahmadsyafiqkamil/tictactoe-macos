56b51a7c95ca9e4cde97463e658101899e37c3c4
ahmadsyafiqkamil <ahmadsyafiqkamil@gmail.com>
2024-05-21 17:45:21 +0700

    Initial Commit

---
+++ tictactoe-macos/tictactoe_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoe_macosApp.swift
+//  tictactoe-macos
+//
+//  Created by Ahmad Syafiq Kamil on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct tictactoe_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
